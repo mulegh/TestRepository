26ae7dcac21eb397d5c6dda2ed2099402aae0a54
mulegh <muleghanshyam@gmail.com>
2017-08-28 14:16:35 +0530

    second commit

---
+++ src/com/firstapplication/HelloWord.java
@@ -0,0 +1,10 @@
+package com.firstapplication;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Welcome to application");
+	}
+
+}
